6401e11adbb26542c63a586524550cae5b221726
lpmarz3 <46807050+lpmarz3@users.noreply.github.com>
2019-02-14 10:13:12 +0900

    181218_001
    당일 학습내용

--- src/study/First.java
+++ src/study/First.java
@@ -1,6 +1,6 @@
 package study;
 
-public class first {
+public class First {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
---
+++ src/study/Var01.java
@@ -0,0 +1,19 @@
+package study;
+
+public class Var01 {
+
+	public static void main(String[] args) {
+		System.out.println("안녕 자바");
+		System.out.println(7);
+		System.out.println(9.8);
+		System.out.println('가');
+		System.out.println(true);
+		System.out.println(false);
+		
+		String name = "정현욱";
+		System.out.println(name);
+		
+
+	}
+
+}
