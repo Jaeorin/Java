91627d14afbea49bba70d04b8f804ddad1cf19ad
lpmarz3 <46807050+lpmarz3@users.noreply.github.com>
2019-02-15 12:51:42 +0900

    오전수업내용

---
+++ src/study_190215/Animal.java
@@ -0,0 +1,15 @@
+package study_190215;
+
+public class Animal {
+
+	String name;
+	int age;
+	// 여기까지 선언
+	
+	public void sound() {
+		System.out.println("나는" + name + "야");
+		System.out.println("나이는" + age + "살이야");
+
+	}
+
+}
---
+++ src/study_190215/MainApp.java
@@ -0,0 +1,14 @@
+package study_190215;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+		Animal a = new Animal();
+		a.name = "È£¶ûÀÌ";
+		a.age = 2;
+		a.sound();
+		
+	}
+
+}
