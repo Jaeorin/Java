0d3d89420eea896ea4240f82dd5e729217490e33
lpmarz3 <46807050+lpmarz3@users.noreply.github.com>
2019-02-21 17:45:16 +0900

    Revert "스타앱 완성"
    This reverts commit 2ebac62a73f75cdfcb5f35a6fcff055d15d0033e.

--- src/study_190220Mk3/Sin.java
+++ src/study_190220Mk3/Sin.java
@@ -1,20 +1,6 @@
 package study_190220Mk3;
 
 public class Sin implements Lamun {
-	
-	// Jin이라는 함수(생성자) 스택 = String name
-	// 아직은 안 만들어졌지만, 곧 만들어짐
-	// 누군가가 new할때, heap으로 저장됨
-	public Sin(String name) {
-		this.name = name;
-	}
-	
-	// 오버로딩
-	public Sin() {
-
-	}
-	
-
 	String name;
 	
 	public void printName() {
