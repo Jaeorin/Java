c04907f0e8662a21fd40c39af882561a55ce8189
lpmarz3 <46807050+lpmarz3@users.noreply.github.com>
2019-02-27 16:28:02 +0900

    수업종료

--- src/report_190220/Hanwoo.java
+++ src/report_190220/Hanwoo.java
@@ -11,18 +11,40 @@ class Globaltest{
 
 public class Hanwoo {
 
-
-	
-	int Local(int a, int b) {
-		int test; // 지역변수
-		test = a + b;
+	int Local1() {
+		int test = 3; // 지역변수
+		System.out.println(test);
 		return test; // 지역변수에서 생성된 값을 호출한 위치로 반환하고자 할 때 사용
+	}
+	
+	int Local2(int a, int b) {
+		int test; 
+		test = a + b;
+		System.out.println(test);
+		return test;
+	}
+	
+	void voidtest(int a) {
+		if(a >= 5){
+			System.out.println(a + "이(가) 5보다 크거나 같다");
+			return;
+		}
+		System.out.println(a + "이(가) 5보다 작다");
 	}
 
 	public static void main(String[] args){
-			
-		Hanwoo localtest1 = new Hanwoo();
-		System.out.println(localtest1.Local(1, 5));
+		
+
+		Hanwoo Localtest = new Hanwoo();
+		Localtest.voidtest(2);	
+		Localtest.voidtest(6);
+		int value1 = Localtest.Local1();
+		int value2 = Localtest.Local2(1,  2);
+		
+		System.out.println(value1 + " + " + value2 + " = " + (value1 + value2));
+		
+		
+		
 		
 		System.out.println("상품 포장용기의 넓비 :" + Globaltest.width);
 		System.out.println("상품 포장용기의 높이 :" + Globaltest.height);
@@ -41,7 +63,8 @@ public class Hanwoo {
 		
 		System.out.println("한우 1번 상품은 " + hanwoo1.rating + "등급에 " + hanwoo1.number
 				+ "조각이며 크기는 가로 " + Globaltest.width + "cm, 세로는 " + Globaltest.height + "cm이다");
-		System.out.println("한우 1번 상품은 " + hanwoo2.rating + "등급에 " + hanwoo2.number
+		
+		System.out.println("한우 2번 상품은 " + hanwoo2.rating + "등급에 " + hanwoo2.number
 				+ "조각이며 크기는 가로 " + Globaltest.width + "cm, 세로는 " + Globaltest.height + "cm이다");
 		
 	}
