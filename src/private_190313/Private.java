b7faa3326ce002891c89bf9b2760d326a89c35b7
lpmarz3 <46807050+lpmarz3@users.noreply.github.com>
2019-03-13 16:59:55 +0900

    수업종료

--- src/private_190313/Private.java
+++ src/private_190313/Private.java
@@ -1,280 +1,319 @@
 package private_190313;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.ArrayList;
-
 
 public class Private {
 
 	public static void main(String[] args) {
 
 		gameFrame gameframe = new gameFrame();
-		
+
 	}
-	
+
 }
 
-
 class gameFrame extends JFrame implements KeyListener, Runnable {
-	
-	int fWidth;
-	int fHeight;
-	
-	int x,y;
-	
+
 	boolean KeyUp = false;
 	boolean KeyDown = false;
 	boolean KeyLeft = false;
 	boolean KeyRight = false;
 	boolean KeyZ = false;
-	
+
+	int fWidth;
+	int fHeight;
+	int x, y;
 	int counter;
-	
-	Thread thread;
-	
-	Toolkit toolkit = Toolkit.getDefaultToolkit();
+	int enemyWidht, enemyHeight;
+	int missileWidht, missileHeight;
+
 	Image meImg;
 	Image missileImg;
 	Image enemyImg;
-	
-	ArrayList missileList = new ArrayList();
-	ArrayList enemyList = new ArrayList();
-	
+
 	Image buffImage;
 	Graphics buffGraphics;
-	
+
 	Missile missile;
 	Enemy enemy;
-	
-	gameFrame(){
-		
+
+	Thread thread;
+
+	Toolkit toolkit = Toolkit.getDefaultToolkit();
+
+	ArrayList missileList = new ArrayList();
+	ArrayList enemyList = new ArrayList();
+
+	gameFrame() {
+
 		init();
 		start();
-		
+
 		setTitle("Test");
 		setSize(fWidth, fHeight);
-		
+
 		Dimension screen = toolkit.getScreenSize();
-		
-		int fXpos = (int)(screen.getWidth() / 2 - fWidth / 2);
-		int fYpos = (int)(screen.getHeight() / 2 - fHeight / 2);
-		
+
+		int fXpos = (int) (screen.getWidth() / 2 - fWidth / 2);
+		int fYpos = (int) (screen.getHeight() / 2 - fHeight / 2);
+
 		setLocation(fXpos, fYpos);
 		setResizable(false);
 		setVisible(true);
-		
+
 	}
-	
+
 	public void init() {
-		
+
 		x = 100;
 		y = 100;
 		fWidth = 470;
 		fHeight = 840;
-		
-		meImg = toolkit.getImage
-				("D:\\workspace\\Java\\src\\private_190313\\cat.png");
-		missileImg = toolkit.getImage
-				("D:\\workspace\\Java\\src\\private_190313\\missile.png");
-		enemyImg = toolkit.getImage
-				("D:\\workspace\\Java\\src\\private_190313\\enemy.png");
-		
+
+		meImg = toolkit.getImage("D:\\workspace\\Java\\src\\private_190313\\cat.png");
+		missileImg = toolkit.getImage("D:\\workspace\\Java\\src\\private_190313\\missile.png");
+		enemyImg = toolkit.getImage("D:\\workspace\\Java\\src\\private_190313\\enemy.png");
+
+		enemyWidht = ImageWidthValue("D:\\workspace\\Java\\src\\private_190313\\enemy.png")
+		enemyHeight = ImageWidthValue
+		missileWidht
+		missileHeight
+
 	}
-	
+
 	public void start() {
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		addKeyListener(this);
 		thread = new Thread(this);
 		thread.start();
-		
+
 	}
-	
+
 	@Override
 	public void run() {
 
-		try{
-			while(true){
+		try {
+			while (true) {
 				keyProcess();
 				missileProcess();
 				enemyProcess();
 				repaint();
 				Thread.sleep(20);
-				
+
 				counter++;
 			}
-		}catch(Exception e){
-				
+		} catch (Exception e) {
+
 		}
 	}
-	
 
-	
 	@Override
 	public void paint(Graphics graphics) {
-		
-		buffImage = createImage(fWidth, fHeight);		
+
+		buffImage = createImage(fWidth, fHeight);
 		buffGraphics = buffImage.getGraphics();
 		update(graphics);
-		
+
 	}
-	
+
 	public void update(Graphics graphics) {
-		
+
 		drawChar();
+		drawEnemy();
 		drawMissile();
-		
+
 		graphics.drawImage(buffImage, 0, 0, this);
-		
+
 	}
-	
+
 	public void drawChar() {
-		
+
 		buffGraphics.clearRect(0, 0, fWidth, fHeight);
 		buffGraphics.drawImage(meImg, x, y, this);
-		
+
 	}
-	
+
+	public void drawEnemy() {
+		for (int i = 0; i < enemyList.size(); ++i) {
+			enemy = (Enemy) (enemyList.get(i));
+			buffGraphics.drawImage(enemyImg, enemy.x, enemy.y, this);
+		}
+	}
+
 	public void drawMissile() {
-		
-		for(int i = 0; i < missileList.size(); i++) {
-			missile = (Missile)(missileList.get(i));
-			buffGraphics.drawImage(missileImg, missile.position.x, missile.position.y, this);
-			
+
+		for (int i = 0; i < missileList.size(); i++) {
+			missile = (Missile) (missileList.get(i));
+			buffGraphics.drawImage(missileImg, missile.x, missile.y, this);
+
 			missile.move();
-			
-			if(missile.position.y > fHeight) {
+
+			if (missile.y > fHeight) {
 				missileList.remove(i);
 			}
 		}
-		
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent event) {
-		
-		switch(event.getKeyCode()) {
-			case KeyEvent.VK_UP :
-				KeyUp = true;
+
+		switch (event.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			KeyUp = true;
 			break;
-			case KeyEvent.VK_DOWN :
-				KeyDown = true;
+		case KeyEvent.VK_DOWN:
+			KeyDown = true;
 			break;
-			case KeyEvent.VK_LEFT :
-				KeyLeft = true;
+		case KeyEvent.VK_LEFT:
+			KeyLeft = true;
 			break;
-			case KeyEvent.VK_RIGHT :
-				KeyRight = true;
+		case KeyEvent.VK_RIGHT:
+			KeyRight = true;
 			break;
-			case KeyEvent.VK_Z :
-				KeyZ = true;
+		case KeyEvent.VK_Z:
+			KeyZ = true;
 			break;
 		}
 	}
-	
-	
 
 	@Override
 	public void keyReleased(KeyEvent event) {
-		
-		switch(event.getKeyCode()){
-			case KeyEvent.VK_UP :
-				KeyUp = false;
+
+		switch (event.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			KeyUp = false;
 			break;
-			case KeyEvent.VK_DOWN :
-				KeyDown = false;
+		case KeyEvent.VK_DOWN:
+			KeyDown = false;
 			break;
-			case KeyEvent.VK_LEFT :
-				KeyLeft = false;
+		case KeyEvent.VK_LEFT:
+			KeyLeft = false;
 			break;
-			case KeyEvent.VK_RIGHT :
-				KeyRight = false;
+		case KeyEvent.VK_RIGHT:
+			KeyRight = false;
 			break;
-			case KeyEvent.VK_Z :
-				KeyZ = false;
+		case KeyEvent.VK_Z:
+			KeyZ = false;
 			break;
 		}
 	}
-	
+
 	@Override
 	public void keyTyped(KeyEvent event) {
-		
+
 	}
-	
+
 	public void keyProcess() {
-		
-		if(KeyUp == true)  y -= 10;
-		if(KeyDown == true) y += 10;
-		if(KeyLeft == true)  x -= 10;
-		if(KeyRight == true) x += 10;
+
+		if (KeyUp == true)
+			y -= 10;
+		if (KeyDown == true)
+			y += 10;
+		if (KeyLeft == true)
+			x -= 10;
+		if (KeyRight == true)
+			x += 10;
 	}
-	
+
 	public void missileProcess() {
-		
-		if(KeyZ) {
+
+		if (KeyZ) {
 			missile = new Missile(x, y);
 			missileList.add(missile);
-			
+
 		}
 
 	}
-	
-	public void enemyProcess(){
 
-		for (int i = 0 ; i < enemyList.size() ; ++i ){
-		enemy = (Enemy)(enemyList.get(i));
-		
-		en.move();
-		if(en.x < -200){
-			Enemy_List.remove(i);
+	public void enemyProcess() {
+
+		for (int i = 0; i < enemyList.size(); ++i) {
+			enemy = (Enemy) (enemyList.get(i));
+
+			enemy.move();
+			if (enemy.y > 900) {
+				enemyList.remove(i);
 			}
 		}
-	
-			if ( cnt % 300 == 0 ){
-				en = new Enemy(f_width + 100, 100);
-				Enemy_List.add(en); 
-				en = new Enemy(f_width + 100, 200);
-				Enemy_List.add(en);
-				en = new Enemy(f_width + 100, 300);
-				Enemy_List.add(en);
-				en = new Enemy(f_width + 100, 400);
-				Enemy_List.add(en);
-				en = new Enemy(f_width + 100, 500);
-				Enemy_List.add(en);
-			}
 
+		if (counter % 300 == 0) {
+			enemy = new Enemy(25, 50);
+			enemyList.add(enemy);
+			enemy = new Enemy(115, 50);
+			enemyList.add(enemy);
+			enemy = new Enemy(205, 50);
+			enemyList.add(enemy);
+			enemy = new Enemy(295, 50);
+			enemyList.add(enemy);
+			enemy = new Enemy(385, 50);
+			enemyList.add(enemy);
 		}
-	
+
+	}
+
+	public int ImageWidthValue(String string) {
+
+		int x = 0;
+		try {
+			File file = new File(string);
+			BufferedImage bufferedimage = ImageIO.read(file);
+
+			x = bufferedimage.getWidth();
+		} catch (Exception e) {
+		}
+		return x;
+	}
+
+	public int ImageHeightValue(String string) {
+
+		int y = 0;
+		try {
+			File file = new File(string);
+			BufferedImage bufferedimage = ImageIO.read(file);
+			y = bufferedimage.getHeight();
+		} catch (Exception e) {
+		}
+		return y;
+	}
+
 }
-
 
 class Missile {
-	
-	Point position;
-	
-	Missile(int x, int y){
-		position = new Point(x, y);
-	}
-	
-	public void move() {
-		position.y -= 10;
-	}
-	
-}
 
-
-class Enemy{
 	int x;
 	int y;
 
-Enemy(int x, int y){
-this.x = x;
-this.y = y;
+	Missile(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public void move() {
+		y -= 10;
+	}
+
 }
-public void move(){
-x -= 3;
-}
+
+class Enemy {
+
+	int x;
+	int y;
+
+	Enemy(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public void move() {
+		y += 3;
+	}
 }
--- src/study_190313/NewsApp.java
+++ src/study_190313/NewsApp.java
@@ -4,6 +4,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
@@ -12,12 +16,57 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 // https://news.naver.com/main/read.nhn?sid1=100&oid=001&aid=0000000001
+public class NewsApp {
 
+	public static void main(String[] args) {
+
+		Crawling craw = new Crawling();
+		String aid = "0000000001";
+		int count = 0;
+
+		while (true) {
+
+			String address = "https://news.naver.com/main/read.nhn?sid1=100&oid=001&aid=" + aid;
+
+			craw.download(address);
+
+			aid = Utils.numberring(aid);
+			count++;
+
+			if (count == 100)
+				break;
+		}
+
+		String sql = null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			sql = "INSERT INTO NEWS(id, address, title, createdDate)" + "VALUES(news_seq.nextval, ?, ?, ?)";
+			con = DBConnection.getConnection();
+			pstmt = con.prepareStatement(sql);
+
+			int size = craw.array.size();
+
+			System.out.println("크기 : " + size);
+
+			for (int i = 0; i < size; i++) {
+				pstmt.setString(1, craw.array.get(i).getAddress());
+				pstmt.setString(2, craw.array.get(i).getTitle());
+				pstmt.setString(3, craw.array.get(i).getcreatedDate());
+				pstmt.executeUpdate();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
 
 class Utils {
 
 	public static String numberring(String aid) {
-	
+
 		// 1. aid를 숫자로 바꾸고
 		// 2. 1증가시키고
 		// 3. 0을 붙여준다
@@ -25,97 +74,108 @@ class Utils {
 		temp++;
 		aid = String.format("%010d", temp);
 		return aid;
-		
+
 	}
 }
-
 
 class News {
 
+	private int id;
 	private String address;
 	private String title;
-	private String creadtedDate;
-	
+	private String createdDate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public String getCreadtedDate() {
-		return creadtedDate;
+
+	public String getcreatedDate() {
+		return createdDate;
 	}
-	public void setCreadtedDate(String creadtedDate) {
-		this.creadtedDate = creadtedDate;
+
+	public void setcreatedDate(String createdDate) {
+		this.createdDate = createdDate;
 	}
-	
+
 }
 
-
 class Crawling {
-	
+
 	ArrayList<News> array = new ArrayList<>();
-	
-	
+
 	// 네이버 뉴스를 다운로드 할 수 있는 함수
 	public void download(String address) {
-		
+
 		StringBuilder html = new StringBuilder();
-		
+
 		try {
 
 			URL url = new URL(address);
 
 			URLConnection con = url.openConnection();
 
-			BufferedReader download = new BufferedReader
-					(new InputStreamReader(con.getInputStream(), "EUC-KR"));
+			BufferedReader download = new BufferedReader(new InputStreamReader(con.getInputStream(), "EUC-KR"));
 
-			while(true) {
+			while (true) {
 				String line = download.readLine();
-				if(line == null) break;
+				if (line == null)
+					break;
 				html.append(line);
 			}
-			
+
 			download.close();
-			
+
 			System.out.println("download end");
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		// html 내용을 파싱해서 articleTitle(id)를 찾아야 함
 		Document doc = Jsoup.parse(html.toString());
-		
+
 		// # : id로 찾음, 스트링으로 가져옴
 		// Elements는 내부적으로 Element를 가지고 있음
 		Elements articleTitle = doc.select("#articleTitle");
 		Elements createDate = doc.select(".t11");
 		Element title = null;
 		Element date = null;
-		
+
 		try {
 			title = articleTitle.get(0);
 			date = createDate.get(0);
-			
+
 			News news = new News();
 			news.setAddress(address);
 			news.setTitle(title.text());
-			news.setCreadtedDate(date.text());
-			
+			news.setcreatedDate(date.text());
+
 			array.add(news);
-			
+
 		} catch (Exception e) {
 			System.out.println("Title NotFound");
 		}
-		
+
 		// . : class로 찾음, 배열로 가져옴
 		doc.select(".tts+head");
 
@@ -130,43 +190,20 @@ class Crawling {
 //		}
 
 	}
-	
+
 }
 
-
-public class NewsApp {
-
-	public static void main(String[] args) {
-		
-		Crawling craw = new Crawling();
-		String aid = "0000000001";
-		
-		int count = 0;
-		
-		while(true) {
-			String address =
-					"https://news.naver.com/main/read.nhn?sid1=100&oid=001&aid=" + aid;
-			craw.download(address);
-			aid = Utils.numberring(aid);
-			
-			count++;
-			
-			if(count == 100) break;
-
+class DBConnection {
+	public static Connection getConnection() {
+		Connection con = null;
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "scott", "tiger");
+			System.out.println("DB Connect Successfully");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("DB Connect Fail");
 		}
-		
-		int size = craw.array.size();
-		
-		System.out.println();
-		
-		for(int i = 0; i < size; i++) {
-			System.out.println(craw.array.get(i).getTitle());
-			System.out.println(craw.array.get(i).getAddress());
-			System.out.println(craw.array.get(i).getCreadtedDate());
-			System.out.println();
-		}
-		
+		return con;
 	}
-
 }
-
